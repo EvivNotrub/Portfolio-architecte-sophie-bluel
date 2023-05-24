3b45870f1d498db544392c56f624f7fb2750d205
EvivNotrub <barthwerle@yahoo.de>
2023-05-24 11:46:42 +0200

    comments in works.js

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -117,6 +117,7 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
 
    /****tools for filters****/
 
+   // this function can target an id or all except id : use the first argument:
    function uncheck(method = "target", value){
         for (let i = 0; i < checkBoxs.length; i++){
             if (method == "except" && checkBoxs[i].dataset.id !== value) {
@@ -126,6 +127,8 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
             }
         };
    }
+   // Below function is used to verify if no checkbox is checked after using them,
+   // otherwise nothing shows on the page:
    function allUnchecked() {
         let state = true;
             for (let i = 0; i < checkBoxs.length; i++) {
@@ -135,7 +138,8 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
             }
         return state
     }
-
+    // can show items of "works" depending wether "all" or id is mentionned,
+    // and unchecks filters in the same time: 
     function show(value = "all"){
         if(value == "all"){
             uncheck("except", "0");
@@ -148,7 +152,7 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
             }
         };
     }
-
+    // can hide items of "works" depending wether "all" or id is mentionned:
     function hide(value = "all"){
         for (let i=0; i<articles.length; i++){
             if (value == "all" || articles[i].dataset.id == value) {
@@ -156,7 +160,7 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
             }
         };
     }
-
+    // the switch:
     function toogle(check, id) {
         if(check.checked == true){
             show(id)
@@ -167,6 +171,14 @@ const checkBoxs = document.querySelectorAll(".filterCheck");
 
 
 /***** main filter function *****/
+
+    /**Below we run through the list of filters and add a listener for each of them
+    in order to take action:
+
+    the marker is used to hide other items from works when first using a filter,
+    but we don't want the hide function again when another category is checked.
+    It needs to be reset when all works are shown.**/
+
 function filter(){    
     let marker = true;
     for (let i = 0; i < checkBoxs.length; i++) {
