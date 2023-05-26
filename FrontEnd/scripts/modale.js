5d4f8d63c3974a44555a5d55aa7048cbd63f31be
EvivNotrub <barthwerle@yahoo.de>
2023-05-27 00:21:11 +0200

    modal can open and close with dedicated links and tab one way

--- FrontEnd/scripts/modale.js
+++ FrontEnd/scripts/modale.js
@@ -1,2 +1,63 @@
+let modal = null;
+const focusableSelector = "button, a, input, textarea";
+let focusables = [];
 
-const authorImg = document.querySelector('.author-img');
+const closeModal = function (event) {
+    if (modal === null) return;
+    event.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").removeEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation);
+    modal = null;
+}
+
+
+const openModal = function (event) {
+    event.preventDefault();
+    // const href = event.target.getAttribute("href");
+    // console.log(href);
+    modal = document.querySelector(this.getAttribute('href'));
+    // console.log(modal);
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    modal.style.display = "";
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+const stopPropagation = function (event) {
+    event.stopPropagation();
+}
+
+const focusInModal = function (event) {
+    event.preventDefault();
+    // console.log(focusables);
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'));
+    console.log(index);
+    index++;
+    if (index >= focusables.length) {
+        index = 0;
+    }
+    focusables[index].focus();
+}
+
+const modalLinks = Array.from(document.querySelectorAll('.js-modal'));
+// console.log(modalLinks);
+modalLinks.forEach( link => {link.addEventListener("click", openModal)});
+
+
+window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+        closeModal(event);
+    }
+    if (event.key === "Tab" && modal !== null) {
+        focusInModal(event);
+    }
+});
+
+
