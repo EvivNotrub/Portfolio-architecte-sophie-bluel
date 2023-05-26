11dc1e5d9f169e5fea5e500e30c35076e3cb0972
EvivNotrub <barthwerle@yahoo.de>
2023-05-26 17:40:03 +0200

    dark mode compatible

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -99,7 +99,7 @@ async function createCategoriesFilters() {
         checkBox.dataset.id = categories[i].id;
         const button = document.createElement("button");
         button.innerText = categories[i].name;
-        button.classList.add("filter-button");
+        button.classList.add("filter-button", "rnd-button");
         filters.appendChild(filter);
         filter.appendChild(checkBox);
         filter.appendChild(button);
