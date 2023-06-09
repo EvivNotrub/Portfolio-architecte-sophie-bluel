de741d0e0ed7fb9333ead54497d44a8530d11085
EvivNotrub <barthwerle@yahoo.de>
2023-06-09 19:44:29 +0200

    minor change console.logs

--- FrontEnd/scripts/modalLink.js
+++ FrontEnd/scripts/modalLink.js
@@ -4,17 +4,15 @@ import { openModal, closeModal, MODAL_TYPE } from './modal.js';
 
 
 export function openModalLinkSetup (openModalLinks, works, container) {
+    console.log("==> openModalLinkSetup\n", "openModalLinks setup EL before :", openModalLinks);
     if (!!openModalLinks) {
-        console.log("openModalLinks setup EL before :");
-        console.log(openModalLinks);
         openModalLinks.forEach( link => {
             link.removeEventListener('click', openModal);
         })
     };
 
     openModalLinks = Array.from(container.querySelectorAll('.js-modal'));
-        console.log("openModalLinks setup EL after :");
-        console.log(openModalLinks);
+        console.log("openModalLinks setup EL after :", openModalLinks);
     openModalLinks.forEach( link => {
         link.removeEventListener('click', openModal);
         link.addEventListener('click', () => openModal(MODAL_TYPE.EDIT_FORM, { data: works }))
@@ -23,16 +21,14 @@ export function openModalLinkSetup (openModalLinks, works, container) {
 }
 
 export function closeModalLinkSetup (closeModalLinks, container) {
+    console.log("==> closeModalLinkSetup\n", "closeModalLinks set EL before :\n", closeModalLinks);
     if (!!closeModalLinks) {
-        console.log("closeModalLinks set EL before:");
-        console.log(closeModalLinks);
         closeModalLinks.forEach( link => {
             link.removeEventListener('click', closeModal);
         })
     };
     closeModalLinks = Array.from(container.querySelectorAll('.js-modal-close'));
-    console.log("closeModalLinks set EL after:");
-    console.log(closeModalLinks);
+    console.log("closeModalLinks set EL after:\n", closeModalLinks);
     closeModalLinks.forEach( link => {
         link.removeEventListener('click', closeModal);
         console.log('===> link', link);
@@ -41,9 +37,7 @@ export function closeModalLinkSetup (closeModalLinks, container) {
     return closeModalLinks;
 }
 export function removeCloseModalLinkSetup (closeModalLinks, container) {
-    console.log("==> removeCloseModalLinkSetup");
-    console.log("closeModalLinks rmvEL before :");
-    console.log(closeModalLinks);
+    console.log("==> removeCloseModalLinkSetup\n", "closeModalLinks rmvEL before :\n", closeModalLinks);
     closeModalLinks.forEach( link => {
         link.removeEventListener('click', closeModal);
     });
