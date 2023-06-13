412ed54f5f17ed2a43ccebeab3602d478beb442c
EvivNotrub <barthwerle@yahoo.de>
2023-06-13 22:57:31 +0200

    custom alert

---
+++ FrontEnd/scripts/alerts.js
@@ -0,0 +1,95 @@
+
+
+const alert = document.getElementById("alertArea");
+const alertBox = document.getElementById("alert_box");
+const alertMessage = document.getElementById("alert__message");
+const alertClose = document.getElementById("alert__close");
+const alertAbort = document.querySelectorAll(".alert__abort");
+const alertConfirm = document.getElementById("alert__confirm");
+const alertAlert = document.getElementById("alert__alert");
+let confirmation = false;
+const delay = timeMs => new Promise(resolve => setTimeout(resolve, timeMs));
+
+async function getPromiseFromAlertEvent(alertAbort, alertConfirm, alertClose, event) {
+    return new Promise(async (resolve) => {
+        
+
+        const listener = (confirmation, value) => {
+            console.log("listener firing!");
+            confirmation = value;
+            alertAbort.forEach(element => {
+                element.removeEventListener(event, listener);
+            });
+            alertConfirm.removeEventListener(event, listener);
+            alertClose.removeEventListener(event, listener);
+            resolve(confirmation);
+        }
+        alertAbort.forEach(element => {
+            element.addEventListener(event, () => {listener(confirmation, false)});
+            });
+
+        alertConfirm.addEventListener(event, () => {listener(confirmation, true)});
+
+        alertClose.addEventListener(event, () => {listener(confirmation, false)});
+
+
+        await delay(30000);
+        resolve(confirmation);
+    })
+  }
+  
+
+export async function customAlert(type = "success", message = {headers: "Attention !", body: ""}) {
+
+    console.log("Alert type: " + type);
+    alert.style.display = "";
+    alert.removeAttribute("aria-hidden");
+    if(type === "info") {
+        alertAbort.forEach(element => {
+            element.style.display = "none";
+        });
+    } else if (type === "warning") {
+        alertAlert.style.display = "";
+        alertClose.style.display = "none";
+    } else if (type === "error") {
+        alertAlert.style.display = "";
+        alertAbort.forEach(element => {
+            element.style.display = "none";
+        });
+    } else if (type === "success") {
+        alertAbort.forEach(element => {
+            element.style.display = "none";
+        });
+    } else {
+        console.error("Alert type not recognized");
+    }
+    if (message.headers === undefined) {
+        message.headers = "Attention !";
+    }
+    alertAlert.innerText = message.headers;
+    alertMessage.innerText = message.body;
+    
+    await getPromiseFromAlertEvent(alertAbort, alertConfirm, alertClose, "click").then( (value) => {
+        confirmation = value;
+        console.log("resolve value" + value);
+    });
+    
+    console.log("confirmation: " + confirmation);
+    closeAlert();
+    return confirmation;
+    
+}
+function closeAlert() {
+    alert.style.display = "none";
+    alert.setAttribute("aria-hidden", "true");
+    alertMessage.innerText = "";
+    alertAlert.style.display = "none";
+    alertClose.style.display = "";
+    alertAbort.forEach(element => {
+        element.removeEventListener("click", closeAlert);
+        element.style.display = "";
+    });
+    alertConfirm.removeEventListener("click", closeAlert);
+    alertClose.removeEventListener("click", closeAlert);
+    
+}
--- FrontEnd/scripts/modalActionButton.js
+++ FrontEnd/scripts/modalActionButton.js
@@ -1,6 +1,7 @@
 import { deleteWork, addWork, getWorks } from "./api.js";
 import { titleInput, categoryInput, addPhotoInput, photoForm, id } from "./modalContentFunctions.js";
 import { openModal } from "./modal.js";
+import { customAlert } from "./alerts.js";
 export let worksEdit = false;
 //in order to be able to change the value of worksEdit from another module:
 
@@ -35,9 +36,6 @@ function validFileSize(files) {
   return valid;
 }
 
-
-
-
 export async function actionAdd(event) {
   event.preventDefault();
   // console.log( addPhotoInput, titleInput, categoryInput);
@@ -49,21 +47,23 @@ export async function actionAdd(event) {
   if(photoForm.reportValidity() && validFileType(files) && validFileSize(files)){      
       console.log("form valid")
      }else{
-      // await customAlert("error", {headers:"Erreur de formulaire !", body: "Merci de bien renseigner tous les champs et de vérifier si l'image est au bon format.."});
       if(files.length === 0){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Oups, vous n'avez pas sélectionné de fichier!"});
         console.log("no file selected");
-      }
-      if(!validFileType(files)){
+      }else if(!validFileType(files)){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Le format de l'image n'est pas valide."});
         console.log("file type not valid");
-      }
-      if(!validFileSize(files)){
+      }else if(!validFileSize(files)){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "La taille de l'image est trop grande."});
         console.log("file size not valid");
-      }
-      if(!titleInput.valid){
+      }else if(!titleInput.valid){
         console.log("Please enter a title");
-      }
-      if(!categoryInput.valid){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Le titre doit être renseigné."});
+      }else if(!categoryInput.valid){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "La catégorie doit être renseignée."});
         console.log("Please choose a category");        
+      }else{
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Merci de bien renseigner tous les champs et de vérifier si l'image est au bon format."});
       }
       return;
      }
@@ -76,22 +76,26 @@ export async function actionAdd(event) {
         // if(!await customAlert("warning", {headers: "Action requise !" , body: "Êtes-vous sûr de vouloir ajouter cette photo ?"})){
         //   return;
         // }
-    if(confirm("Êtes-vous sûr de vouloir ajouter cette photo ?")){
+    // if(confirm("Êtes-vous sûr de vouloir ajouter cette photo ?")){
+      if(await customAlert("warning", {headers: "Action requise !" , body: "Êtes-vous sûr de vouloir ajouter cette photo ?"})){
         const response = await addWork( formDataAdd, token);
             if(response.ok){
               // await customAlert("success", {body: "Votre photo a bien été ajoutée !"});
-              alert("Votre photo a bien été ajoutée !");
+              // alert("Votre photo a bien été ajoutée !");
+              await customAlert("success", {body: "Votre photo a bien été ajoutée !"});
               worksEdit = true;
               console.log("response ok", response);
               Promise.resolve(response);            
             }else{
               console.log("not ok", response);
-              alert("Erreur lors de l'ajout de la photo");
+              // alert("Erreur lors de l'ajout de la photo");
+              await customAlert("error", {headers:"Erreur lors de l'ajout de la photo !", body: "Veuillez réessayer en vérifiant que le fichier est bien au format jpeg ou png et que la taille est inférieure à 4Mo."});
               Promise.reject(response.status);
             return
             }
     }else{
-        alert("Annulation de l'ajout de la photo");
+        // alert("Annulation de l'ajout de la photo");
+        await customAlert("info", {body: "Annulation de l'ajout de la photo"});
         return;
     }
     const works = await getWorks();
@@ -111,21 +115,23 @@ export async function actionEdit(event) {
     if(photoForm.reportValidity() && validFileType(files) && validFileSize(files)){      
       console.log("form valid")
      }else{
-      // await customAlert("error", {headers:"Erreur de formulaire !", body: "Merci de bien renseigner tous les champs et de vérifier si l'image est au bon format.."});
       if(files.length === 0){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Oups, vous n'avez pas sélectionné de fichier!"});
         console.log("no file selected");
-      }
-      if(!validFileType(files)){
+      }else if(!validFileType(files)){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Le format de l'image n'est pas valide."});
         console.log("file type not valid");
-      }
-      if(!validFileSize(files)){
+      }else if(!validFileSize(files)){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "La taille de l'image est trop grande."});
         console.log("file size not valid");
-      }
-      if(!titleInput.valid){
+      }else if(!titleInput.valid){
         console.log("Please enter a title");
-      }
-      if(!categoryInput.valid){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Le titre doit être renseigné."});
+      }else if(!categoryInput.valid){
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "La catégorie doit être renseignée."});
         console.log("Please choose a category");        
+      }else{
+        await customAlert("error", {headers:"Erreur de formulaire !", body: "Merci de bien renseigner tous les champs et de vérifier si l'image est au bon format."});
       }
       return;
      }
@@ -134,7 +140,8 @@ export async function actionEdit(event) {
      formDataAdd.append("image", file);
      formDataAdd.append("title", titleInput.value);
      formDataAdd.append("category", categoryInput.value);
-     if(confirm('Êtes-vous sûr de vouloir remplacer la photo ' + id + '?')){
+    //  if(confirm('Êtes-vous sûr de vouloir remplacer la photo ' + id + '?')){
+      if(await customAlert("warning", {headers: "Action requise !" , body: "Êtes-vous sûr de vouloir remplacer cette photo ?"})){
       const responseDelete = await deleteWork(id, token);
       console.log(responseDelete);
         if(responseDelete.ok){
@@ -144,26 +151,30 @@ export async function actionEdit(event) {
           Promise.resolve(responseDelete);
         }else{
           console.log("photo non supprimée", responseDelete);
-          alert("Erreur lors de la suppression de la photo");
+          // alert("Erreur lors de la suppression de la photo");
+          await customAlert("error", {headers:"Erreur !", body: "La suppression de la photo n'a pas abouti."});
           Promise.reject(responseDelete.status);
           return
         };
         const responseAdd = await addWork( formDataAdd, token);
             if(responseAdd.ok){
               // await customAlert("success", {body: "Votre photo a bien été ajoutée !"});
-              alert("la photo a bien été ajoutée !");
+              // alert("la photo a bien été ajoutée !");
+              await customAlert("success", {body: "Votre photo a bien été remplacée !"});
               worksEdit = true;
               console.log("response ok", responseAdd);
               Promise.resolve(responseAdd);            
             }else{
               console.log("not ok", responseAdd);
-              alert("Erreur lors de l'ajout de la photo");
+              // alert("Erreur lors de l'ajout de la photo");
+              await customAlert("error", {headers:"Erreur !", body: "L'ajout de la photo n'a pas abouti."});
               Promise.reject(responseAdd.status);
             return
             }
         
     }else{
-      alert("Annulation de l'édit de la photo");
+      // alert("Annulation de l'édit de la photo");
+      await customAlert("info", {body: "Annulation de l'édit de la photo"});
       return;
     }
     const works = await getWorks();
@@ -191,23 +202,26 @@ export async function actionDelete(event) {
   const parent = event.target.parentNode.parentNode;
   console.log(parent);
   
-    // if(!await customAlert("warning", {headers: "Action requise !" , body: "Êtes-vous sûr de vouloir supprimer cette photo ?"})){
-    if(confirm('Effacer le projet n°' + id + ' \"' + title + '\" de category : ' + category + ' ?')){
+    if(await customAlert("warning", {headers: "Action requise !" , body: 'Êtes-vous sûr de vouloir supprimer le projet n°' + id + ' \"' + title + '\" de category : ' + category + ' ?'})){
+    // if(confirm('Effacer le projet n°' + id + ' \"' + title + '\" de category : ' + category + ' ?')){
       const response = await deleteWork(id, token);
       console.log(response);
         if(response.ok){
           event.target.parentNode.parentNode.remove();        
-            // await customAlert("success", {body: "Votre photo a bien été supprimée !"});   
-            alert("Votre photo a bien été supprimée !");
+            await customAlert("success", {body: "Votre photo a bien été supprimée !"});   
+            // alert("Votre photo a bien été supprimée !");
           worksEdit = true;     
           Promise.resolve(response);
         }else{
-          console.log("not ok");
+          console.log("not ok", response);
+          // alert("Erreur lors de la suppression de la photo");
+          await customAlert("error", {headers:"Erreur !", body: "La suppression de la photo n'a pas abouti."});
           Promise.reject(response.status);
           return
         }
     }else{
-      alert("Annulation de la suppression de la photo");
+      // alert("Annulation de la suppression de la photo");
+      await customAlert("info", {body: "Annulation de la suppression de la photo!"});
       return;
     }
 
